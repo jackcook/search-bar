674235fa5eac78341eebd1c28f5cc5e03138770c
Jack Cook <jack@jackcook.us>
2014-10-18 18:58:39 -0400

    Implementing best buy

--- stores/bestbuy.js
+++ stores/bestbuy.js
@@ -1,20 +1,23 @@
-var http = require('http');
+var http = require("http");
 
 module.exports = {
-  get_locations: function(product) {
-  	http.get("http://www.google.com/index.html", function(res) {
-	  console.log("Got response: " + res.statusCode);
-	}).on('error', function(e) {
-	  console.log("Got error: " + e.message);
-	});
-    return ['10028', product]; //return array of arrays of coordinates
+  get_locations: function(product, zip) {
+    http.get("http://api.remix.bestbuy.com/v1/products(name=" + product + "*)?show=name,sku&format=json&apiKey=kpxpjpgkvyke9r8aub9urpna", function(res) {
+      var textdata = "";
+      res.on("data", function(chunk) {
+        textdata += chunk;
+      });
+      res.on("end", function() {
+        var data = JSON.parse(textdata);
+        console.log(data.products[0].sku);
+      });
+    });
+    //return ["10028", product]; //return array of arrays of coordinates
   }
 };
 
-
-
-var store = {
+/*var store = {
 	"name": "Best Buy",
 	"lat": "gasrgatwertga",
 	"long": "garfweafawefae"
-};
+};*/
