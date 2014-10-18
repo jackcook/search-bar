39b1bb79fe7b11996bd38b7da5eb14cb3e0bce62
Brian Chuk <brian_chuk@yahoo.com>
2014-10-18 17:53:54 -0400

    progress on bestbuyapi

--- stores/bestbuy.js
+++ stores/bestbuy.js
@@ -1,5 +1,16 @@
+var http = require('http');
+
 module.exports = {
   get_locations: function(product) {
-    return ['10028', product];
+
+    return ['10028', product]; //return array of arrays of coordinates
   }
+};
+
+
+
+var store = {
+	"name": "Best Buy",
+	"lat": "gasrgatwertga",
+	"long": "garfweafawefae"
 };
