be958278bf718afcba7edc4a7579c5545bf1f2ac
Brian Chuk <brian_chuk@yahoo.com>
2014-10-18 17:57:23 -0400

    google api call yay

--- stores/bestbuy.js
+++ stores/bestbuy.js
@@ -2,7 +2,11 @@ var http = require('http');
 
 module.exports = {
   get_locations: function(product) {
-
+  	http.get("http://www.google.com/index.html", function(res) {
+	  console.log("Got response: " + res.statusCode);
+	}).on('error', function(e) {
+	  console.log("Got error: " + e.message);
+	});
     return ['10028', product]; //return array of arrays of coordinates
   }
 };
