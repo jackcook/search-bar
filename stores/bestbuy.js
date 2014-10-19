543d2be822908e676d1cd164c1b3249c6835e5bd
Jack Cook <jack@jackcook.us>
2014-10-18 20:02:04 -0400

    Fetches store objects now

--- stores/bestbuy.js
+++ stores/bestbuy.js
@@ -1,39 +1,37 @@
+var app = require("../app.js");
 var http = require("http");
 
 module.exports = {
   get_locations: function(product, zip) {
     http.get("http://api.remix.bestbuy.com/v1/products(name=" + product + "*)+stores?show=name,sku&format=json&apiKey=kpxpjpgkvyke9r8aub9urpna", function(res) {
-      var producttextdata = "";
+      var nametextdata = "";
       res.on("data", function(chunk) {
-        producttextdata += chunk;
+        nametextdata += chunk;
       });
       res.on("end", function() {
-        var productdata = JSON.parse(producttextdata);
+        var namedata = JSON.parse(nametextdata);
         for (var i = 0; i < 1; i++) {
-          var sku = productdata.products[i].sku;
-          console.log(sku);
-          http.get("http://api.remix.bestbuy.com/v1/products(sku=" + sku + ")+stores?show=sku,stores&format=json&apiKey=kpxpjpgkvyke9r8aub9urpna", function(res) {
-            var storestextdata = "";
+          var sku = namedata.products[i].sku;
+          http.get("http://api.remix.bestbuy.com/v1/products(sku=" + sku + ")+stores?show=name,sku,stores&format=json&apiKey=kpxpjpgkvyke9r8aub9urpna", function(res) {
+            var producttextdata = "";
             res.on("data", function(chunk) {
-              storestextdata += chunk;
+              producttextdata += chunk;
             });
             res.on("end", function() {
-              var storesdata = JSON.parse(storestextdata);
-              for (var i = 0; i < storesdata.products[0].stores.length; i++) {
-                console.log(storesdata.products[0].stores.length);
+              var productdata = JSON.parse(producttextdata);
+              var product = {};
+              product.name = productdata.products[0].name;
+              product.stores = [];
+              var stores = productdata.products[0].stores;
+              for (var i = 0; i < stores.length; i++) {
+                var store = {"name": "Best Buy", "lat": stores[i].lat, "lng": stores[i].lng};
+                product.stores.push(store);
               }
-              //console.log(storesdata.products[0].stores);
+              console.log(product.stores);
             });
           });
         }
       });
     });
-    //return ["10028", product]; //return array of arrays of coordinates
   }
 };
-
-/*var store = {
-	"name": "Best Buy",
-	"lat": "gasrgatwertga",
-	"long": "garfweafawefae"
-};*/
