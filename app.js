b155d2060463848ed03ac58e57ccb6dba2701e43
Jack Cook <jack@jackcook.us>
2014-10-19 01:03:33 -0400

    Working on socket.io implementation

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ var app = require('express')();
 var express = require('express');
 var fs = require('fs');
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var path = require('path');
+
+var allproducts = [];
 
 app.use("/styles", express.static(__dirname));
 
@@ -10,12 +13,24 @@ app.get("/", function(req, res){
   res.sendFile(path.join(__dirname, '../search-bar', 'index.html'));
 });
 
-http.listen(3000, function(){
+app.get("/search.html", function(req, res) {
+  res.sendFile(path.join(__dirname, "../search-bar", "search.html"));
+});
+
+io.on('connection', function(socket) {
+  socket.on('search', function(q, zip) {
+    console.log(zip);
+  });
+});
+
+http.listen(3000, function() {
   var stores = fs.readdirSync('stores');
   for (var i = 0; i < stores.length; i++) {
     var store = require(path.join(__dirname, 'stores', stores[i]));
     store.get_locations('apple', 10003, function(products) {
-      console.log(products.length);
+      for (var j = 0; j < products.length; j++) {
+        allproducts.push(products[j]);
+      }
     });
   }
 });
