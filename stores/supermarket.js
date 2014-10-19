c741f7a02e42b7eb548d599718875982295d7584
Brian Chuk <brian_chuk@yahoo.com>
2014-10-19 00:53:06 -0400

    GELOCATION SWAGGER

--- stores/supermarket.js
+++ stores/supermarket.js
@@ -1,7 +1,12 @@
 var app = require("../app.js");
 var http = require('http');
 var xml2js = require('xml2js');
+var extra = {
+            apiKey: '8aadd6c926121b06baee63e405982545',
+            formatter: null
+};
 
+var geocoder = require('node-geocoder').getGeocoder('opencage', 'http', extra);
 
 module.exports = {
   get_locations: function(product, zip) {
@@ -21,7 +26,22 @@ module.exports = {
           for (var i = 0; i < result.length; i++) {
             var hurr = storeList[i].Storename;
             var durr = storeList;
-            console.dir(durr);
+
+
+            //console.dir(durr);
+
+            var address = '';
+            address += durr[0].Address.toString() + " " + durr[0].City.toString() + " " + durr[0].State.toString() + " " + durr[0].Zip.toString();
+            //console.log(address);
+
+            geocoder.geocode(address, function(err, res) {
+            console.log(res);
+            });
+
+
+
+
+
             http.get("http://www.SupermarketAPI.com/api.asmx/SearchForItem?APIKEY=c4be2f32e1&StoreId=" + result[i].StoreId + "&ItemName=" + product, function(itemRes) {
               var itemTextdata = "";
               itemRes.on("data", function (chunk) {
